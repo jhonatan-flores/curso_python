d58eb06297090d16d8b4ad61604c58399e5ddf91
Jhonatan <jhonatanflorescupe@gmail.com>
2023-05-30 10:17:27 -0500

    realisando un programa de comparacion

---
+++ main/ejercicio2.py
@@ -0,0 +1,17 @@
+#hacer un programa que pida al usuario ingresar su primer apellido,si suapellido tiene como ultimos careteres las letras --ez-- mostrar un mensaje que diga que es un peruano
+##si termina en --es-- eres casi español
+#ingresar dato
+#username=input("ingrese tu primer apellido: ")
+#if username[-2:]=='es':
+#    print("eres casi español: ")
+#elif username[-2:]=='ez':
+#    print("eres casi peruano")
+
+nombreusuario=input("ingrese su DNI: ")
+if len(nombreusuario)==8:
+    sumadedigito=eval(0) + (-1:)
+    print("""
+        ================
+        sumadedigito
+        ================
+        """)
